b5c34e43743c00a228ac30adb2b8232450465b13
Tomasz Dziuda <dziudek@gmail.com>
2012-05-31 22:04:44 +0200

    [v1.28] Added style gk_fashion

---
+++ mod_image_show_gk4/styles/gk_fashion/engine.js
@@ -0,0 +1,115 @@
+window.addEvent("load",function(){
+    $$(".gkIsWrapper-gk_fashion").each(function(el){
+        var elID = el.getProperty("id");
+        var wrapper = $(elID);
+        var $G = $Gavick[elID];
+        var slides = [];
+        var links = [];
+        var imagesToLoad = [];
+        var loadedImages = 0;
+        // animation variables
+        $G['animation_timer']= false;
+        // load the images
+        wrapper.getElements('.gkIsSlide').each(function(el,i){
+            var newImg = new Element('img',{
+                "title":el.getProperty('title'),
+                "class":el.getProperty('class'),
+                "style":el.getProperty('style')
+            });
+            newImg.store('num', i);
+            links[i] = el.getElement('a').getProperty('href');
+            el.getElement('a').destroy();
+            newImg.setProperty("src",el.innerHTML);
+            imagesToLoad.push(newImg);
+            newImg.injectAfter(el);
+            el.destroy();
+        });
+        
+        var time = (function(){
+            var process = 0;                
+            imagesToLoad.each(function(el,i){
+                if(el.complete) process++;
+            });
+            
+            if(process == imagesToLoad.length){
+                $clear(time);
+                loadedImages = process;
+                (function(){
+        			wrapper.getElement('.gkIsPreloader').fade('out');
+        		}).delay(400);
+            }
+        }).periodical(200);
+        
+        var time_main = (function(){
+            if(loadedImages){
+                $clear(time_main);
+                
+                wrapper.getElements(".gkIsSlide").each(function(elmt,i){
+                    slides[i] = elmt;
+                    if($G['slide_links']){
+                        elmt.addEvent("click", function(e){ 
+                            window.location = links[$(e.target).retrieve('num')]; 
+                        });
+                        elmt.setStyle("cursor", "pointer");
+                    }
+                });
+                                
+                $G['actual_slide'] = 0;
+                
+                (function() {
+                	gk_fashion_autoanimate($G, wrapper, 'next');
+                }).delay($G['anim_interval']);
+                // pagination
+                if(wrapper.getElement('ol')) {
+                    wrapper.getElements('ol li').each(function(btn, i) {
+                    	btn.addEvent('click', function() {
+                    		if(i != $G['actual_slide']) {
+                    			gk_fashion_autoanimate($G, wrapper, 'next', i);
+                    		}		
+                    	});
+                    });
+                }
+            }
+        }).periodical(250);
+    });
+});
+
+var gk_fashion_animate = function($G, wrapper, imgPrev, imgNext) {	
+	imgPrev.set('tween', {
+		duration: $G['anim_speed'],
+		onComplete: function() {
+			this.element.setProperty('class', '');
+		}
+	});
+	
+	imgNext.set('tween', {
+		duration: $G['anim_speed'],
+		onComplete: function() {
+			this.element.setProperty('class', 'active');
+			
+			clearTimeout($G['animation_timer']);
+			
+			$G['animation_timer'] = setTimeout(function() {
+				gk_fashion_autoanimate($G, wrapper, 'next', null);
+			},$G['anim_interval']);
+		} 
+	});
+
+	imgPrev.fade('out');
+	imgNext.fade('in');
+}; 
+
+var gk_fashion_autoanimate = function($G, wrapper, dir, nextSlide) {
+	var i = $G['actual_slide'];
+	var imgs = wrapper.getElements('figure');
+	var next = nextSlide;
+	
+	if(nextSlide == null) {
+		next = (dir == 'next') ? ((i < imgs.length - 1) ? i+1 : 0) : ((i == 0) ? imgs.length - 1 : i - 1); // dir: next|prev
+	}
+	
+	gk_fashion_animate($G, wrapper, imgs[i], imgs[next]);
+	$G['actual_slide'] = next;
+	wrapper.getElements('ol li').setProperty('class', '');
+	wrapper.getElements('ol li')[next].setProperty('class', 'active');
+};
---
+++ mod_image_show_gk4/styles/gk_fashion/view.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+* GK Image Show - view file
+* @package Joomla!
+* @Copyright (C) 2009-2012 Gavick.com
+* @ All rights reserved
+* @ Joomla! is Free Software
+* @ Released under GNU/GPL License : http://www.gnu.org/copyleft/gpl.html
+* @ version $Revision: GK4 1.0 $
+**/
+
+// no direct access
+defined('_JEXEC') or die;
+
+$firstFlag = false;
+$firstFlagPag = false;
+
+jimport('joomla.utilities.string');
+
+?>
+
+<div id="gkIs-<?php echo $this->config['module_id'];?>" class="gkIsWrapper-gk_fashion">
+	<div class="gkIsPreloader"></div>
+	
+	<?php for($i = 0; $i < count($this->config['image_show_data']); $i++) : ?>
+	<?php if($this->config['image_show_data'][$i]->published) : ?>
+		<?php 
+			
+			unset($path, $title, $link);
+			// creating slide path
+			$path = $uri->root().'modules/mod_image_show_gk4/cache/'.GKIS_Fashion_Image::translateName($this->config['image_show_data'][$i]->image, $this->config['module_id']);
+			
+            if($this->config['image_show_data'][$i]->type == "k2"){
+              	$title = htmlspecialchars($this->articlesK2[$this->config['image_show_data'][$i]->artK2_id]["title"]);
+                $link =  $this->articlesK2[$this->config['image_show_data'][$i]->artK2_id]["link"];
+            } else {
+                // creating slide title
+				$title = htmlspecialchars(($this->config['image_show_data'][$i]->type == "text") ? $this->config['image_show_data'][$i]->name : $this->articles[$this->config['image_show_data'][$i]->art_id]["title"]);
+				// creating slide link
+				$link = ($this->config['image_show_data'][$i]->type == "text") ? $this->config['image_show_data'][$i]->url : $this->articles[$this->config['image_show_data'][$i]->art_id]["link"];	
+			}
+		?>
+		<figure<?php echo !$firstFlag ? ' class="active"' : ''; ?>>
+			<div class="gkIsSlide" style="z-index: <?php echo $i+1; ?>;" title="<?php echo $title; ?>"><?php echo $path; ?><a href="<?php echo $link; ?>">link</a></div>
+			
+			<?php if($this->config['config']->gk_fashion->gk_fashion_show_title_block) : ?>	
+			<figcaption<?php echo ' class="'.$this->config['config']->gk_fashion->gk_fashion_title_block_position.'"'; ?>>
+				<h3><a href="<?php echo $link; ?>"><?php echo JString::substr($title, 0, $this->config['config']->gk_fashion->gk_fashion_title_block_length); ?></a></h3>
+			</figcaption>
+			<?php endif; ?>
+		</figure>
+	<?php 
+		$firstFlag = true;
+		endif; 
+	?>
+	<?php endfor; ?>
+	
+	<?php if($this->config['config']->gk_fashion->gk_fashion_pagination) : ?>
+	<ol<?php echo ' class="'.$this->config['config']->gk_fashion->gk_fashion_pagination_position.'"'; ?>>
+		<?php for($i = 0; $i < count($this->config['image_show_data']); $i++) : ?>
+		<?php if($this->config['image_show_data'][$i]->published) : ?>
+		<li<?php echo !$firstFlagPag ? ' class="active"' : ''; ?>><?php echo $i; ?></li>
+		<?php 
+			$firstFlagPag = true;
+			endif; 
+		?>
+		<?php endfor; ?>
+	</ol>
+	<?php endif; ?>
+</div>
