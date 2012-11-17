9818e3a59902fce2da3ee7bc6cc453002b2d281b
bkrztuk <bartlomiej.krztuk@gmail.com>
2012-11-17 18:41:42 +0100

    Fix for autoanimation option in gk_fashion style

--- mod_image_show_gk4/styles/gk_fashion/engine.js
+++ mod_image_show_gk4/styles/gk_fashion/engine.js
@@ -55,10 +55,11 @@ window.addEvent("load",function(){
                 });
                                 
                 $G['actual_slide'] = 0;
-                
-                (function() {
-                	gk_fashion_autoanimate($G, wrapper, 'next');
-                }).delay($G['anim_interval']);
+                if($G['autoanim']){	
+	                (function() {
+	                	gk_fashion_autoanimate($G, wrapper, 'next');
+	                }).delay($G['anim_interval']);
+	            }
                 // pagination
                 if(wrapper.getElement('ol')) {
                     wrapper.getElements('ol li').each(function(btn, i) {
