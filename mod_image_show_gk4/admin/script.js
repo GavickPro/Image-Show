0199cbeae1ff0af26c2b153e7f90bfcac0bb6707
dziudek <dziudek@gmail.com>
2013-07-19 14:07:31 +0200

    Added load/save configuration.

--- mod_image_show_gk4/admin/script.js
+++ mod_image_show_gk4/admin/script.js
@@ -4,6 +4,9 @@ $currently_opened = 0;
 window.addEvent("load",function(){
 	// get the updates
 	getUpdates();
+	// initialize the configuration manager
+	var configManager = new ISGK5ConfigManager();
+	// prepare the form
 	var add_form = document.id('gk_tab_add_form');
 	// get the tabs data
 	var tabs = JSON.decode(document.id('jform_params_image_show_data').innerHTML);
@@ -535,3 +538,45 @@ function getUpdates() {
 		}
 	});
 }
+//
+
+// Configuration manager class
+var ISGK5ConfigManager = new Class({
+	// constructor
+	initialize: function() {
+		// create additional variable to avoid problems with the scopes
+		$obj = this;
+		// button load
+		document.id('config_manager_load').addEvent('click', function(e) {
+			e.stop();
+		    $obj.operation('load');
+		});
+		// button save
+		document.id('config_manager_save').addEvent('click', function(e) {
+			e.stop();
+		   	$obj.operation('save');
+		});
+		// button delete
+		document.id('config_manager_delete').addEvent('click', function(e) {
+			e.stop();
+		   	$obj.operation('delete');
+		});
+	},
+	// operation made by the class
+	operation: function(type) {
+		// current url 
+		var current_url = window.location;
+		// check if the current url has no hashes
+		if((current_url + '').indexOf('#', 0) === -1) {
+			// if no - put the variables
+		    current_url = current_url + '&gk_module_task='+type+'&gk_module_file=' + document.id('config_manager_'+type+'_filename').get('value');    
+		} else {
+			// if the url has hashes - remove the hash 
+		    current_url = current_url.substr(0, (current_url + '').indexOf('#', 0) - 1);
+		    // and put the variables
+		    current_url = current_url + '&gk_module_task='+type+'&gk_module_file=' + document.id('config_manager_'+type+'_filename').get('value');
+		}
+		// redirect to the url with variables
+		window.location = current_url;
+	} 
+});
