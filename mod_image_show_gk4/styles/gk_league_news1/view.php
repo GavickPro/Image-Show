af757220634ba599f458502bc1fd126e6f45ac79
bkrztuk <bkrztuk@gavick.com>
2014-03-20 14:13:42 +0100

    Added missing random_slides option
    - small change in options group label from ‘Advanced’ to ‘Advanced
    settings’ to avoid duplicated name in Joomla! 3

--- mod_image_show_gk4/styles/gk_league_news1/view.php
+++ mod_image_show_gk4/styles/gk_league_news1/view.php
@@ -13,6 +13,10 @@
 // no direct access
 defined('_JEXEC') or die;
 
+if($this->config['random_slides'] == 1) {
+	shuffle($this->config['image_show_data']);
+}
+
 ?>
 
 <div id="gkIs-<?php echo $this->config['module_id'];?>" class="gkIsWrapper-gk_league_news1">
