3939f5251277ea6a8e984159cd974f5319094d0c
Tomasz Dziuda <dziudek@gmail.com>
2014-09-03 15:04:45 +0200

    Additional commit for the previous one

---
+++ mod_image_show_gk4/styles/gk_bikestore/class.image.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* @author: GavickPro
+* @copyright: 2008-2012
+**/
+	
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+class GKIS_Bikestore_Image extends GKIS_Image {
+}
+
+/* eof */
