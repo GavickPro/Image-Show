5f33dad8c1972f6bc24a05bc47020b6e0b07b6c0
Tomasz Dziuda <dziudek@gmail.com>
2014-11-19 11:57:53 +0100

    [v.1.49] Added gk_photo style + small fixes

---
+++ mod_image_show_gk4/styles/gk_photo/view.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+* GK Image Show - view file
+* @package Joomla!
+* @Copyright (C) 2009-2012 Gavick.com
+* @ All rights reserved
+* @ Joomla! is Free Software
+* @ Released under GNU/GPL License : http://www.gnu.org/copyleft/gpl.html
+* @ version $Revision: GK4 1.0 $
+**/
+
+// no direct access
+defined('_JEXEC') or die;
+
+jimport('joomla.utilities.string');
+
+if($this->config['random_slides'] == 1) {
+	shuffle($this->config['image_show_data']);
+}
+?>
+
+<div id="gkIs-<?php echo $this->config['module_id'];?>" class="gkIsWrapper-gk_photo">
+	<div class="gkIsPreloader"><span></span></div>
+	
+	<?php for($i = 0; $i < count($this->config['image_show_data']); $i++) : ?>
+	<?php if($this->config['image_show_data'][$i]->published) : ?>
+		<?php 
+			
+			unset($path, $title, $link, $content);
+			// creating slide path
+			$path = '';
+			// check if the slide have to be generated or not
+			if($this->config['generate_thumbnails'] == 1) {
+				$path = $uri->root().'modules/mod_image_show_gk4/cache/'.GKIS_Photo_Image::translateName($this->config['image_show_data'][$i]->image, $this->config['module_id']);
+			} else {
+				$path = $uri->root();
+				$path .= $this->config['image_show_data'][$i]->image;
+			}
+			$content = '';
+			
+            if($this->config['image_show_data'][$i]->type == "k2"){
+              	if(isset($this->articlesK2[$this->config['image_show_data'][$i]->artK2_id])) {
+              		$title = htmlspecialchars($this->articlesK2[$this->config['image_show_data'][$i]->artK2_id]["title"]);
+              		$content = $title;
+                	$link =  $this->articlesK2[$this->config['image_show_data'][$i]->artK2_id]["link"];
+                } else {
+                	$title = 'Selected article doesn\'t exist!';
+                	$content = $title;
+                	$link = '#';
+                }
+            } else {
+                // creating slide title
+				$title = htmlspecialchars(($this->config['image_show_data'][$i]->type == "text") ? $this->config['image_show_data'][$i]->name : $this->articles[$this->config['image_show_data'][$i]->art_id]["title"]);
+				// creating slie content
+				$content = ($this->config['image_show_data'][$i]->type == "text") ? $this->config['image_show_data'][$i]->content : $title;
+				$content = str_replace(array('[leftbracket]', '[rightbracket]', '[ampersand]'), array('<', '>', '&amp;'), $content);
+				// creating slide link
+				$link = ($this->config['image_show_data'][$i]->type == "text") ? $this->config['image_show_data'][$i]->url : $this->articles[$this->config['image_show_data'][$i]->art_id]["link"];	
+			}
+		?>
+		<figure data-url="<?php echo $path; ?>" data-link="<?php echo $link; ?>" data-zindex="<?php echo $i+1; ?>" data-title="<?php echo $title; ?>">
+			<figcaption<?php if($this->config['config']->gk_photo->gk_photo_dark_theme == '1') : ?> data-theme="dark"<?php endif; ?>>
+			<?php if($this->config['config']->gk_photo->gk_photo_show_title_block && $title != '') : ?>	
+				<h2><?php echo $title; ?></h2>
+			
+				<?php if($content != '') : ?>
+				<p><?php echo $content; ?></p>
+				<?php endif; ?>
+				
+				<?php if($link != ''): ?>
+				<?php 
+					$link_text = '';
+					// parsing custom texts
+					$link_match_text = array();
+					
+					if(preg_match('@^\[(.*?)\]@mis', $link, $link_match_text) == 1) {
+						$link = preg_replace('@^\[.*?\]@mis', '', $link);
+						$link_text = $link_match_text[1];
+					} else {
+						$link_text = JText::_('MOD_IMAGE_SHOW_PHOTO_READMORE');
+					}
+				?>
+				<a href="<?php echo $link; ?>"><?php echo $link_text; ?></a>
+				<?php endif; ?>
+			<?php endif; ?>
+			</figcaption>
+		</figure>
+	<?php endif; ?>
+	<?php endfor; ?>
+
+	<?php if($this->config['config']->gk_photo->gk_photo_show_pagination): ?>	
+	<ul class="gkIsPagination">
+	<?php 
+	$j = 0;
+	for($i = 0; $i < count($this->config['image_show_data']); $i++) : ?>
+		<?php if($this->config['image_show_data'][$i]->published) : ?>
+		<li<?php if($j == 0) : ?> class="active"<?php endif; ?>><?php echo $j++; ?></li>
+		<?php endif; ?>
+	<?php endfor; ?>
+	</ul>
+	<?php endif; ?>
+</div>
